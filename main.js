384c556c731195439c6849f9b1c6a45398b4e584
brijesh-kalkani <dev.nikunjbhuva@gmail.com>
2021-11-15 17:49:00 +0530

    changes on main.js

--- main.js
+++ main.js
@@ -8,107 +8,86 @@ app.config(function ($routeProvider) {
       templateUrl: "/components/signup/signup.component.html",
     })
     .when("/", {
-      templateUrl: "/components/login/login.component.html",
+      templateUrl: "/components/home/home.component.html",
     })
-    .when("/home",{
-      templateUrl: "/components/home/home.component.html"
-    }
-    );
+    .when("/home", {
+      templateUrl: "/components/home/home.component.html",
+    });
 });
 
+const redirectTo = (url) => {
+  location.hash = url;
+};
+
+const checkIsLogin = (signup = "#!/login") => {
+  let loginData = JSON.parse(localStorage.getItem("login"));
+  if (loginData) {
+    redirectTo("#!/home");
+    return loginData.username;
+  } else {
+    redirectTo(signup);
+  }
+};
+
+const saveData = (username, password, email, mobilenumber, isFirst) => {
+  let loginData = isFirst ? {}:JSON.parse(localStorage.getItem("loginData"));
+  if (loginData[username]) {
+    console.log('user already exists')
+    return "#!/signup"
+  }
+  loginData[username] = {
+    username: username,
+    password: password,
+    mobilenumber: mobilenumber,
+    email: email,
+  };
+  localStorage.setItem("loginData", JSON.stringify(loginData));
+  return "#!/login"
+};
+
 app.controller("loginCtl", ($scope) => {
+  checkIsLogin();
   $scope.getUser = function () {
     let loginData = JSON.parse(localStorage.getItem("loginData"));
-    // console.log($scope.username)
-    if(loginData){
-      let length = Object.keys(loginData).length;
-      for(let i=0; i < length;i++){
-        if(loginData[i].username == $scope.username && loginData[i].password == $scope.password){
-          console.log("login True")
-          localStorage.setItem('login',JSON.stringify({
-            username:$scope.username,
-            password:$scope.password,
-            key:i
-          }))
-          location.hash = '#!/home'
-        }
-        
+    if (loginData) {
+      if (
+        loginData[$scope.username].username == $scope.username &&
+        loginData[$scope.username].password == $scope.password
+      ) {
+        localStorage.setItem("login",JSON.stringify(loginData[$scope.username]));
+        redirectTo("#!/home");
+      } else {
+        console.log("user does not exist");
       }
     }
   };
 });
 
-app.controller("homeCtl",($scope)=>{
-  let loginData = JSON.parse(localStorage.getItem('login'))
-  if(loginData){
-    $scope.username = loginData.username
-  }else{
-    location.hash ='/login'
-  }
-  $scope.logout = () =>{
-    localStorage.removeItem('login')
-    location.hash = '/'
-  }
-})
-
-const saveData = (username, password, email, mobilenumber, isFirst) => {
-  if (isFirst) {
-    localStorage.setItem(
-      "loginData",
-      JSON.stringify({
-        '0': {
-          username: username,
-          password: password,
-          mobilenumber: mobilenumber,
-          email: email,
-        },
-      })
-    );
-  } else {
-    let loginData = JSON.parse(localStorage.getItem("loginData"));
-    let key1 = Object.keys(loginData).length;
-    loginData[`${key1}`] = {
-      username: username,
-      password: password,
-      mobilenumber: mobilenumber,
-      email: email,
-    };
-
-    localStorage.setItem(
-      "loginData",
-      JSON.stringify({
-        ...loginData
-      })
-    );
-  }
-};
+app.controller("homeCtl", ($scope) => {
+  $scope.username = checkIsLogin();
+  $scope.logout = () => {
+    localStorage.removeItem("login");
+    redirectTo("#!/login");
+  };
+});
 
 app.controller("signupCtl", ($scope) => {
+  checkIsLogin("#!/signup");
   $scope.registerUser = function () {
-    console.log(
-      $scope.username,
-      $scope.password,
-      $scope.email,
+    let loginData = !localStorage.getItem("loginData");
+    if (
+      $scope.username &&
+      $scope.password &&
+      $scope.email &&
       $scope.mobilenumber
-    );
-    if (!localStorage.getItem("loginData") && $scope.username && $scope.password && $scope.email && $scope.mobilenumber) {
-      saveData(
+    ) {
+      redirectTo(saveData(
         $scope.username,
         $scope.password,
         $scope.email,
         $scope.mobilenumber,
-        true
-      );
-      location.hash ='/login'
-    } else {
-      saveData(
-        $scope.username,
-        $scope.password,
-        $scope.email,
-        $scope.mobilenumber,
-        false
-      );
-      location.hash ='/login'
+        loginData
+      ));
     }
   };
 });
